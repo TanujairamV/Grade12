24833f738fd8af2991ba1083b6cca89f717917f2
Tanujairam <tanujairam.v@gmail.com>
2026-01-02 22:39:00 +0530

    Update Table2.sql

--- SQL/Table2.sql
+++ SQL/Table2.sql
@@ -1,3 +1,5 @@
+use practl;
+
 CREATE TABLE Doctors (
     DocID INT,
     DocName VARCHAR(30),
@@ -23,3 +25,40 @@ INSERT INTO Patients VALUES
 (2, 'Naveen', 'Ortho', 101),
 (3, 'Rakesh', 'Neuro', 103),
 (4, 'Atul', 'Physio', 104);
+
+select * from Doctors;
+select * from Patients;
+
+## 1
+select PatNo, PatName, DocName
+from Doctors, Patients
+where Doctors.DocID = Patients.DocID;
+
+## 2
+select count(distinct Department) from Patients;
+
+## 3 
+select * from Doctors 
+where NoofOpdDays > 3;
+
+## 4
+select DocId, Docname from Doctors
+where Department = "ENT";
+
+## 5
+select Doctors.DocId, Doctors.DocName, Doctors.Department, Patients.PatName
+from Doctors, Patients
+where Doctors.DocId = Patients.DocId
+and Doctors.DocId in (101,103);
+
+## 6 
+select * from Doctors
+where DocName like "M%";
+
+## 7 
+select DocID, DocName from Doctors
+where Department = "Ortho";
+
+## 8
+select * from Doctors
+where NoofOpdDays < 3;
