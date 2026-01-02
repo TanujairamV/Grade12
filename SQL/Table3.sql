bd2435fe957cb046ff7cd5e433ca7454fb11866c
Tanujairam <tanujairam.v@gmail.com>
2026-01-02 23:02:24 +0530

    Update Table3.sql

--- SQL/Table3.sql
+++ SQL/Table3.sql
@@ -1,3 +1,5 @@
+use practl;
+
 CREATE TABLE Account (
     ANo INT,
     AName VARCHAR(30),
@@ -25,3 +27,39 @@ INSERT INTO Transact VALUES
 ('T003', 102, 2000, 'Withdraw', '2017-05-12'),
 ('T004', 103, 1000, 'Deposit',  '2017-10-22'),
 ('T005', 102, 12000,'Deposit',  '2017-11-06');
+
+select * from ACCOUNT;
+select * from TRANSACT;
+
+## 1
+select * from TRANSACT
+where TYPE = "Withdraw";
+
+## 2
+select ACCOUNT.ANO, ACCOUNT.ANAME, TRANSACT.AMOUNT, TRANSACT.DOT
+from ACCOUNT, TRANSACT
+where ACCOUNT.ANO = TRANSACT.ANO and TRANSACT.AMOUNT <=3000;
+
+## 3
+select ANO, AMOUNT from TRANSACT 
+where DOT like "%-05-%";
+
+## 4
+select ANO, ANAME from ACCOUNT
+where ADDRESS in ("Chennai","Banglore");
+
+## 5
+select ANO, ANAME from ACCOUNT
+where ANAME like "R%";
+
+## 6
+select distinct ADDRESS from ACCOUNT;
+
+## 7 
+select ACCOUNT.ANO, ACCOUNT.ANAME, TRANSACT.AMOUNT, TRANSACT.DOT 
+from ACCOUNT, TRANSACT
+where ACCOUNT.ANO = TRANSACT.ANO and TRANSACT.AMOUNT >= 3000;
+
+## 8
+select * from TRANSACT
+order by AMOUNT desc;
